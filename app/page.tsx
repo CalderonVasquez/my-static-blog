80c2a764106b3ca027a599d773c36c3b6d547694
EL Marsho <emarsho@gmail.com>
2023-05-02 15:43:07 -0500

    add header and footer

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import Link from 'next/link'
+
+const getPostMetadata = () => {
+    const folder = 'posts/'
+    const files = fs.readdirSync(folder);
+    const markdownPosts = files.filter((file) => file.endsWith(".md"));
+    const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+    return slugs
+}
+
+const HomePage = () => {
+    const postMetadata = getPostMetadata();
+    const postPreviews = postMetadata.map((slug) => (
+        
+            <Link href={`/posts/${slug}`}>
+                <h2>{slug}</h2>
+            </Link>
+    ))
+
+  return (
+    <div>{postPreviews}</div>
+  )
+}
+
+export default HomePage
